770d9da140c907cda176d7cb59d90618c18fdd2e
Daniel Secato <danielsecato99@gmail.com>
2020-04-20 10:13:35 -0300

    Acrescentada uma mensagem de bom dia

--- Repositorio Git/Program.cs
+++ Repositorio Git/Program.cs
@@ -7,6 +7,7 @@ namespace Repositorio_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
             Console.ReadLine();
         }
     }
